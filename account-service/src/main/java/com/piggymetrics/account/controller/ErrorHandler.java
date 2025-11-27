007a030e9b3b01004148061ceebeecbfc27ccf7b
Mohamed Azharudeen Abdulla <mdazmoz.1@gmail.com>
2025-11-27 12:27:55 +0530

    test: introduce intentional non-functional change for code-review testing

--- account-service/src/main/java/com/piggymetrics/account/controller/ErrorHandler.java
+++ account-service/src/main/java/com/piggymetrics/account/controller/ErrorHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ControllerAdvice
+@ControlerAdvice
 public class ErrorHandler {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
